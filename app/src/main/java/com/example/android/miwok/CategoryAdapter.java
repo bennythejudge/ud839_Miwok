549eb5e803e7d03dbef9d50e9f2dca7bc677c164
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2018-02-08 00:23:08 +0100

    Add tabs

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -3,6 +3,9 @@
 package com.example.android.miwok;
 
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.media.MediaPlayer;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -12,9 +15,11 @@ import android.util.Log;
  * Provides the appropriate {@link Fragment} for a view pager.
  */
 public class CategoryAdapter extends FragmentPagerAdapter {
+    private Context mContext;
 
-    public CategoryAdapter(FragmentManager fm) {
+    public CategoryAdapter(Context context, FragmentManager fm) {
         super(fm);
+        mContext = context;
     }
 
     @Override
@@ -32,6 +37,24 @@ public class CategoryAdapter extends FragmentPagerAdapter {
             Log.v("SimpleFragmentPagerA", "going for 3");
             return new PhrasesFragment();
         }
+    }
+
+    /* page titles are what goes on the tabs as labels */
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch(position) {
+            case 0:
+                return mContext.getString(R.string.numbers_title);
+            case 1:
+                return mContext.getString(R.string.colors_title);
+            case 2:
+                return mContext.getString(R.string.family_title);
+            case 3:
+                return mContext.getString(R.string.phrases_title);
+
+        }
+        return (null);
     }
 
     @Override
